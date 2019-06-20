18b3cc63d651d4ff764863a414b56657ed009755
Serega356 <serghomyn@gmail.com>
2019-06-20 20:12:34 +0300

    Resolved merge conflict

--- js/js.js
+++
@@ -1,7 +0,0 @@
-$("#linkedin_logo").click(function () {
-    window.open('https://linkedIn.com', '_blank');
-});
-
-$("img[alt='telegram_logo']").click(function () {
-    window.open('https://telegram.org');
-});
