18706a6845b5c349cc96669e6f42f0ec02937603
Serega356 <serghomyn@gmail.com>
2019-06-21 21:36:54 +0300

    Send form data to server

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const queryString = require('querystring');
+
+http.createServer(function (req, res) {
+
+    if (req.url === "") {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.createReadStream("./public/index.html", "UTF-8").pipe(res);
+    }
+
+    if (req.method === "POST") {
+        let data = "";
+
+        req.on("data", function (chunck) {
+            data += chunck;
+        });
+
+        req.on("end", function () {
+            let formdata = queryString.parse(data);
+            console.log(formdata);
+        });
+    }
+}).listen(63342);
